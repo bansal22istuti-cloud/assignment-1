3eee5cc63d53c4de478b2305baf368433224d821
bansal22istuti-cloud <bansal22istuti@gmail.com>
2025-12-19 17:43:46 +0530

    Add files via upload

---
+++ ass1 task1.py
@@ -0,0 +1,8 @@
+a=float(input("enter the first number:"))
+b=float(input("enter the second number:"))
+print("\n")
+print("addition",a+b)
+print("subtraction",a-b)
+print("multiplication",a*b)
+print("division",a/b)
+input("press enter to exit")
---
+++ ass1 task2.py
@@ -0,0 +1,6 @@
+a=input("enter your first name:")
+b=input("enter your last name:")
+print("\n")
+c=a+" "+b
+print("hello, "+c+"! "+"welcome to the python program")
+input("press enter to exit")
